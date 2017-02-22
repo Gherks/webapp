a136d92db54f36b1ba0986acad01b5051b674529
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2017-02-22 19:09:20 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, render_template
+
+app = Flask("webapp")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+if __name__ == "__main__":
+    app.run()
