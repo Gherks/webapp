d6cd297e4ed412615dcf414bca91336f2b91ea2d
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2017-05-16 10:19:43 +0200

    Update webpage material

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-
 from flask import Flask, render_template
 
 app = Flask("webapp")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+	return render_template("index.html")
 
-@app.route("/info")
-def info():
-    return render_template("info.html")
+@app.route("/info/<username>")
+def info(username):
+	return render_template("info.html", username=username)
 
 if __name__ == "__main__":
-    app.run()
+	app.run("0.0.0.0")
