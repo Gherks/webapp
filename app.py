117d696773910aa0d97fe2deaa7c25ec1f5c2823
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2017-05-18 10:40:06 +0200

    Add contact page to website

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask("webapp")
 
+
 @app.route("/")
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/info/<username>")
 def info(username):
-	return render_template("info.html", username=username)
+    return render_template("info.html", username=username)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
-	app.run("0.0.0.0")
+    app.run("0.0.0.0")
