6c447b5ee002abb5eb1f0c5011f4964dff67f054
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2017-06-28 08:35:47 +0200

    Add login and secret page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
-from forms import MyForm
+from flask import Flask, render_template, redirect, session, url_for
+from forms import ContactForm, LoginForm
 from database import db, Contact
+
+import os
 
 app = Flask("webapp")
 app.config["WTF_CSRF_ENABLED"] = False
-
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "insecure dev key")
 
 @app.before_request
 def before_request():
@@ -19,7 +21,15 @@ def after_request(response):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    visits = int(session.get("number_of_visits", 0))
+    visits += 1
+    session["number_of_visits"] = visits
+    welcome_message = ""
+    if visits == 1:
+        welcome_message = "This is your first visit!"
+    else:
+        welcome_message = "You have visited this site {} times.".format(visits)
+    return render_template("index.html", welcome_message=welcome_message)
 
 
 @app.route("/info/<username>")
@@ -29,7 +39,7 @@ def info(username):
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    form = MyForm()
+    form = ContactForm()
     if form.validate_on_submit():
         first_name = form.first_name.data
         last_name = form.last_name.data
@@ -44,6 +54,30 @@ def contact():
 def show_db():
     entries = Contact.select()
     return render_template("show_db.html", entries=entries)
+
+@app.route("/secrets", methods=["GET"])
+def secrets():
+    print("TRYING TO ENTER SECRETS")
+    if "logged_in" in session.keys():
+        print("KEY EXISTS")
+        if session["logged_in"] == 1:
+            print("SUCCESSFULLY LOGGED IN!")
+            return render_template("secrets.html")
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.password.data == "asd":
+            session["logged_in"] = 1
+            return redirect(url_for('secrets'))
+    return render_template("login.html", form=form)
+    
+@app.route("/logout", methods=["GET"])
+def logout():
+    session["logged_in"] = 0
+    return login()
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, PasswordField, validators
 
 
-class MyForm(FlaskForm):
+class ContactForm(FlaskForm):
     first_name = StringField('First name', [validators.InputRequired()])
     last_name = StringField('Last name', [validators.InputRequired()])
     miscellaneous = StringField('Miscellaneous', [validators.InputRequired()])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.InputRequired()])
+    password = PasswordField('Password', [validators.InputRequired()])
